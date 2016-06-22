092b93d7e1ecabe5661c3a830d6772c9cf2e6774
Nina Yang <nina.yang@appdynamics.com>
2016-06-22 15:20:30 -0700

    added ejs

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'LunchDynamics' });
+  res.render('index.ejs', { title: 'LunchDynamics' });
 });
 
 module.exports = router;
