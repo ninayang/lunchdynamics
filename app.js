1276ff0fdf389e242fd03ea89cfe7f14e806996b
Eugene Oh <eugene.oh@appdynamics.com>
2016-06-22 16:03:28 -0700

    View engine modified for HTML files

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 app.use(logger('dev'));
@@ -40,10 +41,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
+    res.render('error.html');
   });
 }
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var path = require("path");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index.ejs', { title: 'LunchDynamics' });
+  res.render('index.html');
 });
 
 module.exports = router;
